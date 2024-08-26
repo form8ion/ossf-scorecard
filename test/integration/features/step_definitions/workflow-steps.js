272986a007624b1b08c59b772f1e64fbb94b0ded
Matt Travi <programmer@travi.org>
2024-08-26 11:55:09 -0500

    test(workflow-file): check for existence with workflows-core checker

--- test/integration/features/step_definitions/workflow-steps.js
+++ test/integration/features/step_definitions/workflow-steps.js
@@ -1,8 +1,10 @@
 import {promises as fs} from 'node:fs';
-import {fileExists} from '@form8ion/core';
+import {workflowFileExists} from '@form8ion/github-workflows-core';
 
 import {Given, Then} from '@cucumber/cucumber';
 import {assert} from 'chai';
+
+const scorecareWorkflowName = 'scorecard';
 
 Given('Actions workflows exist', async function () {
   await fs.mkdir(`${this.projectRoot}/.github/workflows`, {recursive: true});
@@ -13,9 +15,9 @@ Given('no Actions workflows exist', async function () {
 });
 
 Then('the workflow is defined', async function () {
-  assert.isTrue(await fileExists(`${this.projectRoot}/.github/workflows/scorecard.yml`));
+  assert.isTrue(await workflowFileExists({projectRoot: this.projectRoot, name: scorecareWorkflowName}));
 });
 
 Then('the workflow is not defined', async function () {
-  assert.isFalse(await fileExists(`${this.projectRoot}/.github/workflows/scorecard.yml`));
+  assert.isFalse(await workflowFileExists({projectRoot: this.projectRoot, name: scorecareWorkflowName}));
 });
